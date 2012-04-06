cc4fb30cf4d8214ed4218a2619b2415067ac4936
Mike_Zobac <mikezobal@gmail.com>
2012-04-06 16:45:51 -0700

    c++ version of audio output

---
+++ client/src/AudioOutput.cpp
@@ -0,0 +1,214 @@
+
+#include "AudioOutput.h"
+#include <stdio.h>
+
+
+AudioOutput::AudioOutput()
+{
+    initialize();
+}
+
+
+AudioOutput::~AudioOutput()
+{
+    cleanUp();
+}
+
+
+void AudioOutput::writeAudio(HWAVEOUT waveOut, LPSTR data, int size)
+{
+    WAVEHDR* current;
+    int remain;
+    current = &(globals->blocks[globals->currentBlock]);
+
+    while(size > 0) 
+    {
+
+        /* Make sure the header we're going to use is unprepared */
+        if(current->dwFlags & WHDR_PREPARED) 
+            waveOutUnprepareHeader(waveOut, current, sizeof(WAVEHDR));
+
+        if(size < (int)(BLOCK_SIZE - current->dwUser)) 
+        {
+            memcpy(current->lpData + current->dwUser, data, size);
+            current->dwUser += size;
+            break;
+        }
+
+        remain = BLOCK_SIZE - current->dwUser;
+        memcpy(current->lpData + current->dwUser, data, remain);
+        size -= remain;
+        data += remain;
+        current->dwBufferLength = BLOCK_SIZE;
+        waveOutPrepareHeader(waveOut, current, sizeof(WAVEHDR));
+
+        waveOutWrite(waveOut, current, sizeof(WAVEHDR));
+
+        EnterCriticalSection(&(globals->countGuard));
+        globals->freeBlocks--;
+        LeaveCriticalSection(&(globals->countGuard));
+
+        /* wait for a block to become free */
+        while(!globals->freeBlocks)
+            Sleep(10);
+
+        /*point to the next block*/
+        globals->currentBlock++;
+        globals->currentBlock %= BLOCK_COUNT;
+        current = &(globals->blocks[globals->currentBlock]);
+        current->dwUser = 0;
+    }
+}
+
+
+void AudioOutput::playAudio()
+{
+    
+    /* try to open the default wave device  */
+    if(waveOutOpen(&(globals->waveOut), WAVE_MAPPER, &(globals->wfx), (DWORD_PTR)waveOutProc, (DWORD_PTR)globals, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) 
+    {
+        fprintf(stderr, "unable to open wave mapper device\n");
+        ExitProcess(1);
+    }
+
+    CreateThread(NULL, 0, AudioOutput::playProc, this, 0, NULL);
+}
+
+
+
+DWORD WINAPI AudioOutput::playProc(LPVOID lpParameter)
+{
+    
+    AudioOutput *audio = (AudioOutput*)lpParameter;
+    DWORD bytesRead;
+        
+    /* playback loop */
+    while(1) 
+    {
+
+        bytesRead = audio->globals->nc->receiveMulticast(&(audio->globals->buffer));
+
+        if(bytesRead < sizeof(audio->globals->buffer)) 
+        {
+            memset(audio->globals->buffer.buf + bytesRead, 0, BUFSIZE - bytesRead);
+        }
+
+        audio->writeAudio(audio->globals->waveOut, audio->globals->buffer.buf, BUFSIZE);
+    }
+
+    /* wait for all blocks to complete */
+    while(audio->globals->freeBlocks < BLOCK_COUNT)
+        Sleep(10);
+}
+
+
+void AudioOutput::getTitles()
+{
+    DWORD bytesRead;
+
+    bytesRead = globals->nc->receiveData(&(globals->buffer));
+    memcpy(globals->titles, &(globals->buffer), bytesRead);
+    return;
+}
+
+
+WAVEHDR* AudioOutput::allocateBlocks(int size, int count)
+{
+    unsigned char* buffer;
+    int i;
+    WAVEHDR* blocks;
+    DWORD totalBufferSize = (size + sizeof(WAVEHDR)) * count;
+
+    /* allocate memory for the entire set in one go */
+    if((buffer = (unsigned char*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, totalBufferSize)) == NULL)
+    {
+        fprintf(stderr, "Memory allocation error\n");
+        ExitProcess(1);
+    }
+
+    /* and set up the pointers to each bit */
+    blocks = (WAVEHDR*)buffer;
+    buffer += sizeof(WAVEHDR) * count;
+
+    for(i = 0; i < count; i++)
+    {
+        blocks[i].dwBufferLength = size;
+        blocks[i].lpData = (LPSTR)buffer;
+        buffer += size;
+    }
+    return blocks;
+}
+
+
+void AudioOutput::freeBlocks(WAVEHDR* blockArray)
+{
+    /* and this is why allocateBlocks works the way it does */ 
+    HeapFree(GetProcessHeap(), 0, blockArray);
+}
+
+
+void CALLBACK AudioOutput::waveOutProc(HWAVEOUT waveOut, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
+{
+    /* pointer to free block counter */
+    PGLOBALS globals = (PGLOBALS)dwInstance;
+
+    /* Ignore all calls except when a block is finished playing */
+    if(uMsg != WOM_DONE)
+        return;
+    EnterCriticalSection(&(globals->countGuard));
+    (globals->freeBlocks)++;
+    LeaveCriticalSection(&(globals->countGuard));
+}
+
+
+void AudioOutput::cleanUp()
+{
+    size_t  i;
+
+    /* Unprepare the blocks before freeing the data buffer */
+    for(i = 0; i < globals->freeBlocks; i++)
+    {
+        if(globals->blocks[i].dwFlags & WHDR_PREPARED)
+            waveOutUnprepareHeader(globals->waveOut, &(globals->blocks)[i], sizeof(WAVEHDR));
+    }
+
+    DeleteCriticalSection(&(globals->countGuard));
+    freeBlocks(globals->blocks);
+    waveOutClose(globals->waveOut);
+}
+
+
+void AudioOutput::getHeaderData()
+{
+    /*after three way handshake, TCP message
+    will have header data.  read it into WAVEFORMATEX struct*/
+    
+    globals->wfx.nSamplesPerSec = 44100; /*sample rate*/
+    globals->wfx.wBitsPerSample = 16; /*sample size*/
+    globals->wfx.nChannels= 2;  /*channels*/
+    globals->wfx.cbSize = 0;  /*size of extra info*/
+    globals->wfx.wFormatTag = WAVE_FORMAT_PCM;
+    globals->wfx.nBlockAlign = (globals->wfx.wBitsPerSample * globals->wfx.nChannels) >> 3;
+    globals->wfx.nAvgBytesPerSec = globals->wfx.nBlockAlign * globals->wfx.nSamplesPerSec;
+}
+
+
+int AudioOutput::connect(char *host, unsigned short socket)
+{
+    return globals->nc->connectToServer(host, socket);
+}
+
+void AudioOutput::initialize()
+{
+    globals = (PGLOBALS)malloc(sizeof(GLOBALS));
+    
+    /* initialize globals */ 
+    globals->blocks = allocateBlocks(BLOCK_SIZE, BLOCK_COUNT);
+    globals->freeBlocks = BLOCK_COUNT;
+    globals->currentBlock= 0;
+    InitializeCriticalSection(&(globals->countGuard));
+    globals->nc = new NetworkingComponent(NetworkingComponent::CLIENT);
+    globals->nc->initialize();
+}
+
+
---
+++ client/src/AudioOutput.h
@@ -0,0 +1,54 @@
+#ifndef AUDIOOUTPUT_H
+#define AUDIOOUTPUT_H
+
+
+#include "NetworkingComponent.h"
+#include <windows.h>
+#include <mmsystem.h>
+#include <string>
+
+#pragma comment(lib, "WinMM.Lib")
+
+#define BLOCK_SIZE 8192
+#define BUFSIZE    1024
+#define BLOCK_COUNT 20
+
+typedef struct _globals
+{
+    HWAVEOUT            waveOut;
+    WAVEHDR             *blocks;
+    WAVEFORMATEX        wfx;
+    int                 freeBlocks;
+    int                 currentBlock, len;
+    WSABUF              buffer;
+    CRITICAL_SECTION    countGuard;
+    char                *titles;
+    NetworkingComponent* nc;
+
+}GLOBALS, *PGLOBALS;
+
+
+class AudioOutput{
+
+public:
+
+    AudioOutput();
+    ~AudioOutput();
+    WAVEHDR* allocateBlocks(int size, int count);
+    void freeBlocks(WAVEHDR* blockArray);
+    void writeAudio(HWAVEOUT waveOut, LPSTR data, int size);
+    void playAudio();
+    void cleanUp();
+    void initialize();
+    void getHeaderData();
+    void getTitles();
+    int connect(char *host, unsigned short socket);
+    static void CALLBACK waveOutProc(HWAVEOUT, UINT, DWORD, DWORD, DWORD);
+    static DWORD WINAPI playProc(LPVOID lpParameter);
+
+private:
+    
+    PGLOBALS    globals;
+};
+
+#endif
