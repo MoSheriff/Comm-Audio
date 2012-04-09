7f11baf9c73fa75ba6077fc4d0c6b1f320ecfe39
Mike_Zobac <mikezobal@gmail.com>
2012-04-09 15:10:18 -0700

    added playing and playlist handling

--- client/src/wndproc.cpp
+++ client/src/wndproc.cpp
@@ -2,6 +2,8 @@
 #include "AudioOutput.h"
 #include "main.h"
 
+std::list<std::string> getTitles(NetworkingComponent *nc);
+
 LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	HWND listenButton, quitButton, skipButton, songListBox, clientListBox,
@@ -14,7 +16,8 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	PAINTSTRUCT ps;
     static std::list<std::string> titles;
     std::list<std::string>::iterator it;
-	int i, cxClient, cyClient;
+	int i, j, cxClient, cyClient;
+    WSABUF  buffer;
 
      switch (message)
      {
@@ -30,16 +33,16 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
          output = new AudioOutput();
          output->setNc(nc);
 
-         listenButton = CreateWindow("button", "|>", WS_CHILD|WS_VISIBLE, 65, 480, 40, 40, hwnd, (HMENU)LISTEN_BUTTON, NULL, NULL);
-		 quitButton = CreateWindow("button", "||",  WS_CHILD|WS_VISIBLE, 110, 480, 40, 40, hwnd, (HMENU)LISTEN_BUTTON, NULL, NULL);
-		 skipButton = CreateWindow("button", ">>",  WS_CHILD|WS_VISIBLE, 155, 480, 40, 40, hwnd, (HMENU)LISTEN_BUTTON, NULL, NULL);
+         listenButton = CreateWindow("button", "Listen", WS_CHILD|WS_VISIBLE, 65, 480, 40, 40, hwnd, (HMENU)LISTEN_BUTTON, NULL, NULL);
+		 quitButton = CreateWindow("button", "Silence",  WS_CHILD|WS_VISIBLE, 110, 480, 40, 40, hwnd, (HMENU)LISTEN_BUTTON, NULL, NULL);
+		 skipButton = CreateWindow("button", "Skip",  WS_CHILD|WS_VISIBLE, 155, 480, 40, 40, hwnd, (HMENU)LISTEN_BUTTON, NULL, NULL);
 
-		 requestButton = CreateWindow("button", "Request",  WS_CHILD|WS_VISIBLE, 100, 475, 80, 20, hwnd, (HMENU)REQUEST_BUTTON, NULL, NULL);
+		 requestButton = CreateWindow("button", "Request",  WS_CHILD|WS_VISIBLE, 100, 445, 80, 20, hwnd, (HMENU)REQUEST_BUTTON, NULL, NULL);
 		 updateButton = CreateWindow("button", "Update",  WS_CHILD|WS_VISIBLE, 600, 435, 100, 20, hwnd, (HMENU)UPDATE_BUTTON, NULL, NULL);
 		 callButton = CreateWindow("button", "Call",  WS_CHILD|WS_VISIBLE, 560, 460, 80, 20, hwnd, (HMENU)CALL_BUTTON, NULL, NULL);
 		 endcallButton = CreateWindow("button", "End Call",  WS_CHILD|WS_VISIBLE, 660, 460, 80, 20, hwnd, (HMENU)ENDCALL_BUTTON, NULL, NULL);
 
-		 songListBox = CreateWindow("listbox", NULL, WS_CHILD | WS_VISIBLE |WS_BORDER| LBS_NOTIFY, 20, 20, 240, 450, hwnd,(HMENU)SONG_LISTBOX, NULL, NULL);
+		 songListBox = CreateWindow("listbox", NULL, WS_CHILD | WS_VISIBLE |WS_BORDER| LBS_NOTIFY, 20, 20, 240, 420, hwnd,(HMENU)SONG_LISTBOX, NULL, NULL);
 		 clientListBox = CreateWindow("listbox", NULL, WS_CHILD | WS_VISIBLE |WS_BORDER| LBS_NOTIFY, 530, 250, 240, 180, hwnd,(HMENU)CLIENT_LISTBOX, NULL, NULL);
 		 break;
 
@@ -55,23 +58,15 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		 {
 		 
          case ID_SERVER_CONNECT:
-             
-             output->connect(hostName, NetworkingComponent::LISTENPORT);
-             
-             // use the return value of this to display the available titles
-             titles = getTitles();
-             
-             for (it = titles.begin(); it != titles.end(); ++it)
-             {
-                 SendDlgItemMessage(hwnd, CLIENT_LISTBOX, LB_ADDSTRING, 0, (LPARAM)*it->c_str()); //listbox test
-             }
-
              break;
 		 
          case ID_SERVER_DISCONNECT:
 			 break;
 		 
          case LISTEN_BUTTON:
+
+             output->playAudio();
+
 			 break;
 		 
          case LEAVE_BUTTON:
@@ -81,13 +76,33 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 			 break;
 		 
          case REQUEST_BUTTON:
-			 break;
+             
+             i = SendMessage(hwnd, LB_GETCURSEL, NULL, NULL);
+             j = SendMessage(hwnd, LB_GETITEMDATA, (WPARAM)i, NULL);
+
+             if(j == LB_ERR)
+                 break;
+
+             buffer.buf = (char *)j;
+
+             nc->sendData(buffer.buf, buffer.len);
+             
+             break;
 		 
          case UPDATE_BUTTON:
 			 SendDlgItemMessage(hwnd, CLIENT_LISTBOX, LB_ADDSTRING, 0, (LPARAM)"test"); //listbox test
 			 break;
 		 
          case CALL_BUTTON:
+             output->connect("127.0.0.1", NetworkingComponent::LISTENPORT);
+
+             // use the return value of this to display the available titles
+             titles = getTitles(nc);
+
+             for (it = titles.begin(); it != titles.end(); ++it)
+             {
+                 SendDlgItemMessage(hwnd, CLIENT_LISTBOX, LB_ADDSTRING, 0, (LPARAM)*it->c_str()); //listbox test
+             }
 			 break;
 		 
          case ENDCALL_BUTTON:
